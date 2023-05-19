99ce1217ab3d7ae460febaf0fd912f826a5cdbb1
Lucian Miller <lucian.miller185@gmail.com>
2023-05-19 11:05:12 -0700

    Add business logic for game start and making guesses

--- WordPuzzle.Solution/WordPuzzle/Controllers/GamesController.cs
+++ WordPuzzle.Solution/WordPuzzle/Controllers/GamesController.cs
@@ -7,16 +7,34 @@ namespace WordPuzzle.Controllers
 {
   public class GamesController : Controller
   {
-    // [Route("/games")]
+    [HttpGet("/games")]
     public ActionResult Index()
     {
+      if (Game.Instance == null)
+      {
+        Game newGame = new Game();
+        newGame.GameStart();
+        System.Console.WriteLine("-----------");
+        System.Console.WriteLine(newGame.Word);
+        System.Console.WriteLine(String.Join(" ", newGame.CharList));
+        System.Console.WriteLine(String.Join(" ", newGame.EmptyList));
+        System.Console.WriteLine("-----------");
+      }
       return View();
     }
 
     [HttpPost]
     public ActionResult Index(string userInput)
     {
-      System.Diagnostics.Debug.WriteLine("This will be displayed in output window");
+      if (char.TryParse(userInput, out char c))
+      {
+        Game.Instance.MakeGuess(c);
+      }
+      System.Console.WriteLine("-----------");
+      System.Console.WriteLine("Correct: " + String.Join(" ", Game.Instance.CorrectList));
+      System.Console.WriteLine("Incorrect: " + String.Join(" ", Game.Instance.IncorrectList));
+      System.Console.WriteLine(String.Join(" ", Game.Instance.EmptyList));
+      System.Console.WriteLine("-----------");
       return View("Index");
     }
   }
--- WordPuzzle.Solution/WordPuzzle/Models/Game.cs
+++ WordPuzzle.Solution/WordPuzzle/Models/Game.cs
@@ -1,10 +1,67 @@
 using System.Collections.Generic;
+using System;
+using System.Linq;
+
+// step 5: stop game when 6 incorrect guesses have been guessed or user correctly guesses letter
+// step 6: prevent user from typing more then one letter or unaccepted characters 
 
 namespace WordPuzzle.Models
 {
   public class Game
   {
+    public static Game Instance { get; set; }
     public string UserInput { get; set; }
     public string Word { get; set; }
+    public List<Char> CharList { get; set; } = new List<Char>();
+    public List<Char> EmptyList { get; set; } = new List<Char>();
+    public List<Char> CorrectList { get; set; } = new List<Char>();
+    public List<Char> IncorrectList { get; set; } = new List<Char>();
+
+    public Game()
+    {
+      if (Instance == null) {
+        Instance = this;
+      }
+    }
+
+    public void GameStart()
+    {
+      Random random = new Random();
+      List<String> wordDictionary = new List<String> {"asteroid", "astronaut", "astronomer", "atmosphere", "comet", "constellation", "corona", "cosmos", "crater", "eclipse", "equinox", "force", "galaxy", "gravity", "hemisphere", "meteor", "meteorite", "moon", "nebula", "observatory", "orbit", "planet", "pulsar", "quasar", "space", "star", "sun", "universe"};
+      int index = random.Next(wordDictionary.Count);
+      String randomWord = wordDictionary[index];
+      Word = randomWord;
+      CharList.AddRange(Word);
+      foreach (char c in CharList)
+      {
+        EmptyList.Add('_');
+      }
+    }
+
+    public void MakeGuess(char guess)
+    {
+      if (Word.Contains(guess)) 
+      {
+        CorrectList.Add(guess);
+        AddCorrectGuess(guess);
+      }
+      else
+      {
+        IncorrectList.Add(guess);
+      }
+    }
+
+    private void AddCorrectGuess(char correctGuess)
+    {
+      for (int index = 0; index < CharList.Count; index++) 
+      {
+        if (correctGuess == CharList[index])
+        {
+          EmptyList[index] = correctGuess;
+        }
+      }
+    }
+
+    private void 
   }
 }
