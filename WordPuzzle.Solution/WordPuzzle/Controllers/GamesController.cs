45710fc008326122538cfa66fd49b9a0c6c613cb
Lucian Miller <lucian.miller185@gmail.com>
2023-05-30 09:08:45 -0700

    Remove development code

--- WordPuzzle.Solution/WordPuzzle/Controllers/GamesController.cs
+++ WordPuzzle.Solution/WordPuzzle/Controllers/GamesController.cs
@@ -14,11 +14,6 @@ namespace WordPuzzle.Controllers
       {
         Game newGame = new Game();
         newGame.GameStart();
-        System.Console.WriteLine("-----------");
-        System.Console.WriteLine(newGame.Word);
-        System.Console.WriteLine(String.Join(" ", newGame.CharList));
-        System.Console.WriteLine(String.Join(" ", newGame.EmptyList));
-        System.Console.WriteLine("-----------");
       }
       return View(Game.Instance);
     }
@@ -30,23 +25,9 @@ namespace WordPuzzle.Controllers
       {
         Game.Instance.MakeGuess(Char.ToLower(c));
       }
-      System.Console.WriteLine("-----------");
-      System.Console.WriteLine("Correct: " + String.Join(" ", Game.Instance.CorrectList));
-      System.Console.WriteLine("Incorrect: " + String.Join(" ", Game.Instance.IncorrectList));
-      System.Console.WriteLine(String.Join(" ", Game.Instance.EmptyList));
-      System.Console.WriteLine("-----------");
       if (Game.Instance.Win || Game.Instance.Lose)
       {
-        Console.WriteLine("Game Over!");
         return View("GameOver", Game.Instance);
-        // if (Game.Instance.Win)
-        // {
-        //   Console.WriteLine("You Win!");
-        // }
-        // else
-        // {
-        //   Console.WriteLine("You Lose!");
-        // }
       }
       return View("Index", Game.Instance);
     }
